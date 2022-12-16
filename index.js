78cc0e8a3b102f8a219966f725f3408279e8d585
aisha-jufe <aishajucfe287>
2022-12-16 11:07:47 +0300

    completd assignment

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
-
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
@@ -11,7 +12,22 @@ function renderBooks(books) {
     main.appendChild(h2);
   });
 }
-
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
+}
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
+}
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
